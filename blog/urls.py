2caca1c5e63334030e634273c8100d5f55b821bb
Harsh Modi <harshmodi2000@yahoo.in>
2024-11-07 21:10:39 +0530

    [IMP]

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.blog_index, name="blog_index"),
+    path("login", views.blog_login, name="blog_login"),
     path("post/<int:pk>/", views.blog_detail, name="blog_detail"),
     path("category/<category>/", views.blog_category, name="blog_category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from blog.models import Post, Comment
+from blog.forms import CommentForm
 
 # Create your views here.
 
@@ -23,9 +25,24 @@ def blog_category(request, category):
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post,
+            )
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
     context = {
         "post": post,
         "comments": comments,
+        "form": CommentForm(),
     }
 
     return render(request, "blog/details.html", context)
+
+def blog_login(request):
+    return render(request, "blog/login.html")
